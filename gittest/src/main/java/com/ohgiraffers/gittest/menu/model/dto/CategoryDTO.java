bc19f5226e18263a0f0ea1f61ff84bf2c0750275
YYSSJJIINN <h20171058@daum.net>
2025-03-04 14:34:38 +0900

    [feat] main브랜치 기본 틀 완성
    기초 토대 완성

---
+++ gittest/src/main/java/com/ohgiraffers/gittest/menu/model/dto/CategoryDTO.java
@@ -0,0 +1,8 @@
+package com.ohgiraffers.gittest.menu.model.dto;
+
+public class CategoryDTO {
+
+    private int code;
+    private String name;
+    private int refCategoryCode;
+}
---
+++ gittest/src/main/java/com/ohgiraffers/gittest/menu/model/dto/MenuDTO.java
@@ -0,0 +1,10 @@
+package com.ohgiraffers.gittest.menu.model.dto;
+
+public class MenuDTO {
+
+    private int code;
+    private String name;
+    private int price;
+    private int categoryCode;
+    private String orderableStatus;
+}
